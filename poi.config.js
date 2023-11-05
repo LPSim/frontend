4ada3cdb998a02b7110a84b7018e869b3d7fedda
zyr17 <jzjqz17@gmail.com>
2023-11-05 19:57:43 +0800

    remove unused packages

--- poi.config.js
+++ poi.config.js
@@ -18,9 +18,5 @@ module.exports = {
   },
   presets: [
     require('poi-preset-bundle-report')(),
-    require('poi-preset-offline')({
-      pwa: './src/pwa.js', // Path to pwa runtime entry
-      pluginOptions: {} // Additional options for offline-plugin
-    })
   ]
 }
